8b360de89c02c2bf046ac2a986316b446abfa30e
Head Gardener <trashbin2019np@gmail.com>
2024-01-29 19:50:46 +0300

    direnv, sotu

--- flake.nix
+++ flake.nix
@@ -8,20 +8,27 @@
   outputs = { self, nixpkgs, flake-utils }:
     let
       version = "0.1.${nixpkgs.lib.substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
-      nixpkgsFor = system:
-        import nixpkgs {
-          inherit system;
-          overlays = nixpkgs.lib.attrsets.attrValues self.overlays;
-        };
     in
     {
       overlays.default = import ./overlay.nix version;
 
       nixosModules.default = self.nixosModules.blog;
       nixosModules.blog = import ./module.nix;
-    } // flake-utils.lib.eachDefaultSystem (system: {
-      packages = {
-        inherit (nixpkgsFor system) blog-render;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs {
+        inherit system;
+        overlays = nixpkgs.lib.attrsets.attrValues self.overlays;
       };
-    });
+      in
+      {
+        packages = {
+          inherit (pkgs) blog-render;
+        };
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (pkgs.writeScriptBin "serve"
+              "${pkgs.python3}/bin/python3 -m http.server 8000 -d result/")
+          ];
+        };
+      });
 }
